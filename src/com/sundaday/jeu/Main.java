15d511b76e51bc21a8aee6a9bbd91aad01e6bba6
sundaday <mc_s1@hotmail.com>
2020-07-18 17:44:20 +0200

    test

---
+++ src/com/sundaday/jeu/Main.java
@@ -0,0 +1,35 @@
+package com.sundaday.jeu;
+
+
+import java.util.Scanner;
+
+class Main {
+    static Scanner entrée = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int tempMin = entrée.nextInt();
+        int tempMax = entrée.nextInt();
+
+        for (int i = tempMin; i <= tempMax; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
